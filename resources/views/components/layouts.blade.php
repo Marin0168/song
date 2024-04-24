2f14ca4a996e0b6b691d06896054c5a53b33f955
Marino01 <81763040+Marino01@users.noreply.github.com>
2024-04-24 11:24:45 +0200

    nieuws

--- resources/views/components/layouts.blade.php
+++ resources/views/components/layouts.blade.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{$title}}</title>
-    <script src="https://cdn.tailwindcss.com" ></script>
+    <!-- <script src="https://cdn.tailwindcss.com" ></script> -->
+    @vite('resources/css/app.css') 
 </head>
 <body>
-<!-- @vite('resources/css/app.css')  -->
 
 <div class="min-h-full">
   <nav class="bg-gray-800">
--- resources/views/songs.blade.php
+++ resources/views/songs.blade.php
@@ -5,7 +5,7 @@
 
 
     @foreach($songs as $song)
-    <div class="my-4">
+    <div class="my-4 bg-red-400">
         <a href="{{ route('show', $song->id) }}" class="block hover:bg-gray-100 rounded-lg p-4 transition duration-300 ease-in-out">
             <strong class="text-blue-500">Titel:</strong> {{ $song->title }} <br>
             <span class="text-gray-600">Singer: {{ $song->singer }}</span>
